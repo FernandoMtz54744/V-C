033c878df0f1ab21307c83a21f6cca8d54f599f5
Fernando <fer_f@outlook.com>
2022-08-03 14:19:28 -0500

    Seccion 7

---
+++ Seccion 7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toogleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li>Phone: <strong>{{friend.phone}}</strong></li>
+            <li>Email: <strong>{{friend.email}}</strong></li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                detailsAreVisible: false,
+                friend: {
+                        id: "Friend1",
+                        name: "nameFriend1",
+                        phone: "5565122061",
+                        email: "Friend1@gmail.com"
+                    }
+            }
+        },
+        methods:{
+            toogleDetails(){
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    }
+</script>

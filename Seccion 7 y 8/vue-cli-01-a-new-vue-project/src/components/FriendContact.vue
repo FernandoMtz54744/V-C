e4e5e73758a31c47bec8857e26b2a09753242ced
Fernando <fer_f@outlook.com>
2022-08-04 16:57:59 -0500

    Seccion 8

---
+++ Seccion 7 y 8/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>{{friend.name}}  {{friend.isFavorite ? "(Favorite)" : ""}}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <button @click="toggleFavorite" >favorite</button>
+        <ul v-if="detailsAreVisible">
+            <li>Phone: <strong>{{friend.phone}}</strong></li>
+            <li>Email: <strong>{{friend.email}}</strong></li>
+        </ul>
+         <button @click="$emit('delete', friend.id);">Delete</button>
+    </li>
+   
+</template>
+
+<script>
+    export default {
+        emits: ["toggle-favorite", "delete"], 
+        props: ["friend"],
+        data(){
+            return{
+                detailsAreVisible: false
+            }
+        },
+        methods:{
+            toggleDetails(){
+                this.detailsAreVisible = !this.detailsAreVisible;
+            },
+            toggleFavorite(){
+                this.$emit("toggle-favorite", this.friend.id);
+            }
+        }
+    }
+</script>

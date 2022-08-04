25a3d5c9f918840b64b03de1258c4b8afc6d3d35
Fernando <fer_f@outlook.com>
2022-08-04 18:16:06 -0500

    Tarea 06

---
+++ Seccion 7 y 8/Tarea 06/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul>
+        <li>
+            <h2>Username: {{user.username}}</h2>
+             <h3>Age: {{user.age}}</h3>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ["user"]
+}
+</script>
